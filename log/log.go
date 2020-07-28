470568eef0b482175fb1ae6ba94f8f341b11998e
olongfen <olongfen@localhost.localdomain>
2020-07-28 17:55:29 +0800

    chore: fix time

--- log/log.go
+++ log/log.go
@@ -4,8 +4,9 @@ import (
 	"fmt"
 	rotatelogs "github.com/lestrrat-go/file-rotatelogs"
 	"github.com/rifflock/lfshook"
-	log "github.com/sirupsen/logrus"
+	logrus "github.com/sirupsen/logrus"
 	"io"
+	"log"
 	"os"
 	"path/filepath"
 	"runtime"
@@ -13,18 +14,376 @@ import (
 	"time"
 )
 
+// Log flag
 var (
-	Log = New()
+	// Log default
+	Log = New() // default
+	// RotationCount count
+	RotationCount uint = 365 //
 )
 
+// Logger log struct
 type Logger struct {
-	*log.Logger
+	*logrus.Logger
+	// local
+	LogFlag      int // 日志标签 非DEBUG方法
+	LogFlagDebug int // 日志 DEBUG方法
+}
+
+// SetFlags log normal
+func SetFlags(flg int) {
+	Log.SetFlags(flg)
+}
+
+// SetLevel log normal
+func SetLevel(level uint32) {
+	Log.SetLevel(level)
+}
+
+// Debug debug
+func Debug(args ...interface{}) {
+	if Log.Level >= logrus.DebugLevel {
+		Log.EntryWith(Log.LogFlag).Debug(args...)
+	}
+}
+
+// Debugf debug
+func Debugf(format string, args ...interface{}) {
+	if Log.Level >= logrus.DebugLevel {
+		Log.EntryWith(Log.LogFlag).Debugf(format, args...)
+	}
+}
+
+// Debugln debug
+func Debugln(args ...interface{}) {
+	if Log.Level >= logrus.DebugLevel {
+		Log.EntryWith(Log.LogFlag).Debugln(args...)
+	}
+}
+
+// Info info
+func Info(args ...interface{}) {
+	if Log.Level >= logrus.InfoLevel {
+		Log.EntryWith(Log.LogFlag).Info(args...)
+	}
+}
+
+// Infof info
+func Infof(format string, args ...interface{}) {
+	if Log.Level >= logrus.InfoLevel {
+		Log.EntryWith(Log.LogFlag).Infof(format, args...)
+	}
+}
+
+// Infoln info
+func Infoln(args ...interface{}) {
+	if Log.Level >= logrus.InfoLevel {
+		Log.EntryWith(Log.LogFlag).Infoln(args...)
+	}
+}
+
+// Warn warn
+func Warn(args ...interface{}) {
+	if Log.Level >= logrus.WarnLevel {
+		Log.EntryWith(Log.LogFlag).Warn(args...)
+	}
+}
+
+// Warnf warn
+func Warnf(format string, args ...interface{}) {
+	if Log.Level >= logrus.WarnLevel {
+		Log.EntryWith(Log.LogFlag).Warnf(format, args...)
+	}
+}
+
+// Warnln warn
+func Warnln(args ...interface{}) {
+	if Log.Level >= logrus.WarnLevel {
+		Log.EntryWith(Log.LogFlag).Warnln(args...)
+	}
+}
+
+// Error error
+func Error(args ...interface{}) {
+	if Log.Level >= logrus.ErrorLevel {
+		Log.EntryWith(Log.LogFlag).Error(args...)
+	}
+}
+
+// Errorf error
+func Errorf(format string, args ...interface{}) {
+	if Log.Level >= logrus.ErrorLevel {
+		Log.EntryWith(Log.LogFlag).Errorf(format, args...)
+	}
+}
+
+// Errorln error
+func Errorln(args ...interface{}) {
+	if Log.Level >= logrus.ErrorLevel {
+		Log.EntryWith(Log.LogFlag).Errorln(args...)
+	}
+}
+
+// Fatal fatal
+func Fatal(args ...interface{}) {
+	if Log.Level >= logrus.FatalLevel {
+		Log.EntryWith(Log.LogFlag).Fatal(args...)
+	}
+}
+
+// Fatalf fatal
+func Fatalf(format string, args ...interface{}) {
+	if Log.Level >= logrus.FatalLevel {
+		Log.EntryWith(Log.LogFlag).Fatalf(format, args...)
+	}
+}
+
+// Fatalln fatal
+func Fatalln(args ...interface{}) {
+	if Log.Level >= logrus.FatalLevel {
+		Log.EntryWith(Log.LogFlag).Fatalln(args...)
+	}
+}
+
+// Panic panic
+func Panic(args ...interface{}) {
+	Log.EntryWith(Log.LogFlag).Panic(args...)
+}
+
+// Panicf panic
+func Panicf(format string, args ...interface{}) {
+	Log.EntryWith(Log.LogFlag).Panicf(format, args...)
+}
+
+// Panicln panic
+func Panicln(args ...interface{}) {
+	Log.EntryWith(Log.LogFlag).Panicln(args...)
+}
+
+// Print print
+func Print(args ...interface{}) {
+	logrus.NewEntry(Log.Logger).Print(args...)
+}
+
+// Printf print
+func Printf(format string, args ...interface{}) {
+	logrus.NewEntry(Log.Logger).Printf(format, args...)
+}
+
+// Println print
+func Println(args ...interface{}) {
+	logrus.NewEntry(Log.Logger).Println(args...)
+}
+
+// SetFlags log normal
+func (l *Logger) SetFlags(flg int) {
+	l.LogFlag |= flg
+	l.LogFlagDebug = l.LogFlag
+}
+
+// SetLevel log normal
+func (l *Logger) SetLevel(level uint32) {
+	l.Level = logrus.Level(level)
+}
+
+// GetLevel log normal
+func (l *Logger) GetLevel() uint32 {
+	return uint32(l.Level)
+}
+
+// Upon up on
+func (l *Logger) Upon(level uint32) (ret bool) {
+	if uint32(l.Level) >= level {
+		ret = true
+	}
+	return
+}
+
+// Debug debug
+func (l *Logger) Debug(args ...interface{}) {
+	if l.Level >= logrus.DebugLevel {
+		l.EntryWith(l.LogFlagDebug).Debug(args...)
+	}
+}
+
+// Debugf debug
+func (l *Logger) Debugf(format string, args ...interface{}) {
+	if l.Level >= logrus.DebugLevel {
+		l.EntryWith(l.LogFlagDebug).Debugf(format, args...)
+	}
+}
+
+// Debugln debug
+func (l *Logger) Debugln(args ...interface{}) {
+	if l.Level >= logrus.DebugLevel {
+		l.EntryWith(l.LogFlagDebug).Debugln(args...)
+	}
+}
+
+// Info info
+func (l *Logger) Info(args ...interface{}) {
+	if l.Level >= logrus.InfoLevel {
+		l.EntryWith(l.LogFlag).Info(args...)
+	}
+}
+
+// Infof info
+func (l *Logger) Infof(format string, args ...interface{}) {
+	if l.Level >= logrus.InfoLevel {
+		l.EntryWith(l.LogFlag).Infof(format, args...)
+	}
+}
+
+// Infoln info
+func (l *Logger) Infoln(args ...interface{}) {
+	if l.Level >= logrus.InfoLevel {
+		l.EntryWith(l.LogFlag).Infoln(args...)
+	}
+}
+
+// Warn warn
+func (l *Logger) Warn(args ...interface{}) {
+	if l.Level >= logrus.WarnLevel {
+		l.EntryWith(l.LogFlag).Warn(args...)
+	}
+}
+
+// Warnf warn
+func (l *Logger) Warnf(format string, args ...interface{}) {
+	if l.Level >= logrus.WarnLevel {
+		l.EntryWith(l.LogFlag).Warnf(format, args...)
+	}
+}
+
+// Warnln warn
+func (l *Logger) Warnln(args ...interface{}) {
+	if l.Level >= logrus.WarnLevel {
+		l.EntryWith(l.LogFlag).Warnln(args...)
+	}
+}
+
+// Error error
+func (l *Logger) Error(args ...interface{}) {
+	if l.Level >= logrus.ErrorLevel {
+		l.EntryWith(l.LogFlag).Error((args)...)
+	}
+}
+
+// Errorf error
+func (l *Logger) Errorf(format string, args ...interface{}) {
+	if l.Level >= logrus.ErrorLevel {
+		l.EntryWith(l.LogFlag).Errorf(format, (args)...)
+	}
+}
+
+// Errorln error
+func (l *Logger) Errorln(args ...interface{}) {
+	if l.Level >= logrus.ErrorLevel {
+		l.EntryWith(l.LogFlag).Errorln((args)...)
+	}
+}
+
+// Print print
+func (l *Logger) Print(args ...interface{}) {
+	l.EntryWith(l.LogFlag).Print(args...)
+}
+
+// Printf print
+func (l *Logger) Printf(format string, args ...interface{}) {
+	l.EntryWith(l.LogFlag).Printf(format, args...)
+}
+
+// Println print
+func (l *Logger) Println(args ...interface{}) {
+	l.EntryWith(l.LogFlag).Println(args...)
+}
+
+// Fatal fatal
+func (l *Logger) Fatal(args ...interface{}) {
+	if l.Level >= logrus.FatalLevel {
+		l.EntryWith(l.LogFlag).Fatal(args...)
+	}
+}
+
+// Fatalf fatal
+func (l *Logger) Fatalf(format string, args ...interface{}) {
+	if l.Level >= logrus.FatalLevel {
+		l.EntryWith(l.LogFlag).Fatalf(format, args...)
+	}
+}
+
+// Fatalln fatal
+func (l *Logger) Fatalln(args ...interface{}) {
+	if l.Level >= logrus.FatalLevel {
+		l.EntryWith(l.LogFlag).Fatalln(args...)
+	}
+}
+
+// Panic panic
+func (l *Logger) Panic(args ...interface{}) {
+	if l.Level >= logrus.PanicLevel {
+		l.EntryWith(l.LogFlag).Panic(args...)
+	}
+}
+
+// Panicf panic
+func (l *Logger) Panicf(format string, args ...interface{}) {
+	if l.Level >= logrus.PanicLevel {
+		l.EntryWith(l.LogFlag).Panicf(format, args...)
+	}
+}
+
+// Panicln panic
+func (l *Logger) Panicln(args ...interface{}) {
+	if l.Level >= logrus.PanicLevel {
+		l.EntryWith(l.LogFlag).Panicln(args...)
+	}
+}
+
+// EntryWith 格式化输出
+func (l *Logger) EntryWith(flg int) *logrus.Entry {
+	if flg&(log.Lshortfile|log.Llongfile) != 0 {
+		if _, file, line, ok := runtime.Caller(0); ok {
+			return l.Logger.WithFields(logrus.Fields{
+				"detail": fmt.Sprintf("%s|%d", file, line),
+			})
+		}
+	}
+
+	return logrus.NewEntry(l.Logger)
+}
+
+// Close 关闭
+func (l *Logger) Close() error {
+	if l.Out != nil {
+		if w, ok := l.Out.(io.WriteCloser); ok {
+			return w.Close()
+		}
+	}
+	return nil
+}
+
+// Copy 复制
+func (l *Logger) Copy() (r *Logger) {
+	//r = new(Logger)
+	//*r = *l
+	//r.Logger = logrus.New()
+	//*r.Logger = *l.Logger
+	//r.Logger.Out = l.Logger.Out
+	r = New()
+	r.SetLevel(uint32(l.Level))
+	r.Out = l.Out
+	return
 }
 
 func New() *Logger {
-	return &Logger{
-		Logger: log.New(),
-	}
+	d := &Logger{}
+	d.Logger = logrus.New()
+	// default
+	d.SetFlags(log.Llongfile)
+	d.SetLevel(uint32(logrus.DebugLevel))
+	d.Out = os.Stderr
+	return d
 }
 
 // NewLogFile new log file
@@ -55,24 +414,24 @@ func NewLogFile(logPath string, isPrint bool) (d *Logger) {
 		if isPrint {
 			d.Hooks.Add(lfshook.NewHook(
 				lfshook.WriterMap{
-					log.TraceLevel: rf,
-					log.DebugLevel: rf,
-					log.InfoLevel:  rf,
-					log.WarnLevel:  rf,
+					logrus.TraceLevel: rf,
+					logrus.DebugLevel: rf,
+					logrus.InfoLevel:  rf,
+					logrus.WarnLevel:  rf,
 					// log.ErrorLevel: rf,
-					log.FatalLevel: rf,
-					log.PanicLevel: rf,
+					logrus.FatalLevel: rf,
+					logrus.PanicLevel: rf,
 				},
-				&log.JSONFormatter{},
+				&logrus.JSONFormatter{},
 			))
 		} else {
 			d.Out = rf
 		}
 
 	} else {
-		log.Warnln(err)
+		logrus.Warnln(err)
 	}
-	d.SetFormatter(&log.JSONFormatter{})
+	d.SetFormatter(&logrus.JSONFormatter{})
 	// hook errors
 	d.AddHook(&HookError{Filepath: logPath + ".error"})
 
@@ -90,18 +449,18 @@ type HookError struct {
 }
 
 // Levels level
-func (h *HookError) Levels() []log.Level {
-	return []log.Level{log.ErrorLevel}
+func (h *HookError) Levels() []logrus.Level {
+	return []logrus.Level{logrus.ErrorLevel}
 }
 
-func (h *HookError) SetFormat(logger *log.Logger, format log.Formatter) *HookError {
+func (h *HookError) SetFormat(logger *logrus.Logger, format logrus.Formatter) *HookError {
 	logger.SetFormatter(format)
 	return h
 }
 
 // Fire fire
-func (h *HookError) Fire(entry *log.Entry) error {
-	if entry.Level == log.ErrorLevel && len(h.Filepath) > 0 {
+func (h *HookError) Fire(entry *logrus.Entry) error {
+	if entry.Level == logrus.ErrorLevel && len(h.Filepath) > 0 {
 		if _s, _err := entry.String(); _err == nil {
 			if _, _err = os.Stat(h.Filepath); os.IsNotExist(_err) && h.Out != nil {
 				h.Out.Close()
